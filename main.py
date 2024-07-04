48710bffc9ce542e86ffb2d8ec2276a5daacd4cd
matthewglasser <matthewsglasser@gmail.com>
2024-07-04 10:25:57 -0400

    view training data

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# mnist = tf.keras.datasets.mnist
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print(x_train[0])
+plt.imshow(x_train[0], cmap=plt.cm.binary)
+plt.show()
 
 # x_train = tf.keras.utils.normalize(x_train, axis=1)
 # x_test = tf.keras.utils.normalize(x_test, axis=1)
@@ -22,18 +26,18 @@ import tensorflow as tf
 
 # model.save('handwritten.keras')
 
-model = tf.keras.models.load_model('handwritten.keras')
+# model = tf.keras.models.load_model('handwritten.keras')
 
-image_number = 1
-while os.path.isfile(f"digits/digit{image_number}.PNG"):
-  try:
-    img = cv2.imread(f"digits/digit{image_number}.PNG")[:,:,0]
-    img = np.invert(np.array([img]))
-    prediction = model.predict(img)
-    print(f"Digit {image_number}: {np.argmax(prediction)}")
-    plt.imshow(img[0], cmap=plt.cm.binary)
-    plt.show()
-  except:
-    print("Error")
-  finally:
-    image_number += 1
+# image_number = 1
+# while os.path.isfile(f"digits/digit{image_number}.PNG"):
+#   try:
+#     img = cv2.imread(f"digits/digit{image_number}.PNG")[:,:,0]
+#     img = np.invert(np.array([img]))
+#     prediction = model.predict(img)
+#     print(f"Digit {image_number}: {np.argmax(prediction)}")
+#     plt.imshow(img[0], cmap=plt.cm.binary)
+#     plt.show()
+#   except:
+#     print("Error")
+#   finally:
+#     image_number += 1
